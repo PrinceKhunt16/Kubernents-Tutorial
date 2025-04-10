20297c9563fdbd742fe0c36a897cda80438bbef1
Prince Khunt <princekhuntdatastructure@gmail.com>
2025-04-10 22:45:26 +0530

    Added deployment.yaml, docker and webapp

--- app.py
+++ app.py
@@ -5,10 +5,12 @@ app = Flask(__name__)
 @app.route("/", methods=["GET", "POST"])
 def home():
     message = ""
+
     if request.method == "POST":
         name = request.form.get("name")
         message = f"Hello {name}, Welcome to the Kubernetes Tutorial application!"
+
     return render_template("index.html", message=message)
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000)
+    app.run(host="0.0.0.0", port=5001)
